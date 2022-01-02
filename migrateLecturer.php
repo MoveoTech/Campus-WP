e8f4c7166450ea3a3aa9d695438ad44f068ca32f
aliKhMoveo <alik@moveo.co.il>
2022-01-02 11:07:22 +0200

    migrate data

--- migrateLecturer.php
+++ migrateLecturer.php
@@ -13,109 +13,63 @@ $wp_query = new WP_Query(array(
     'posts_per_page' => -1,
     'post_status' => 'publish',
 ));
-echo nl2br("number of lecturer =  $wp_query->found_posts \n"); //admin: 406,  WP_Query: 325
+
+echo nl2br("number of lecturer =  $wp_query->found_posts \n"); //admin: 414,  WP_Query: 414
 $posts = $wp_query->posts;
 
-$hebrew_ids = array();
-$english_ids = array();
-$arabic_ids = array();
+
+$ids = array();
 
 foreach($posts as $result) {
 
-    $translated1 = apply_filters( 'wpml_object_id', $result->ID, 'academic_institution', FALSE, 'he' );
-    if ($translated1){
-        array_push($hebrew_ids, $translated1);
-    }
+    array_push($ids, array(
+        "he"=>apply_filters( 'wpml_object_id', $result->ID, 'academic_institution', FALSE, 'he' ),
+        "en"=>apply_filters( 'wpml_object_id', $result->ID, 'academic_institution', FALSE, 'en' ),
+        "ar"=>apply_filters( 'wpml_object_id', $result->ID, 'academic_institution', FALSE, 'ar' )
+    ));
 
-    $translated2 = apply_filters( 'wpml_object_id', $result->ID, 'academic_institution', FALSE, 'en' );
-    if ($translated2){
-        array_push($english_ids, $translated2);
-    }
-    $translated3 = apply_filters( 'wpml_object_id', $result->ID, 'academic_institution', FALSE, 'ar' );
-    if ($translated3){
-        array_push($arabic_ids, $translated3);
-    }
 }
 
 
-
-for($i=0 ; $i <  count($hebrew_ids) ; $i++)
+for($i=0 ; $i <  count($ids) ; $i++)
 {
-    $result = get_post($hebrew_ids[$i]);
-    $resultEnglish = get_post($english_ids[$i]);
-    $resultArabic = get_post($arabic_ids[$i]);
-
+    $resultHebrew = get_post($ids[$i]['he']);
+    $resultEnglish = $ids[$i]['en']? get_post($ids[$i]['en']) : null;
+    $resultArabic = $ids[$i]['ar']? get_post($ids[$i]['ar']) : null;
+//    if($resultHebrew->ID == 8275){
     ?>
 
-    <div class="restaurant-card" style="border: 1px solid red; min-height: 150px; padding: 10px">
-        <h4>lecturer Number :</h4><p> <?php echo $index++; ?></p>
-                <h4>ID :</h4><p class="name"> <?php echo $result->ID; ?></p>
-        <h4>Name : </h4><p class="name"><?php echo $result->post_title; ?></p>
-                <h4>Author ID :</h4><p class="name"> <?php echo $result->post_author; ?></p>
-                <h4>Description : </h4><p class="name"><?php echo $result->post_content; ?></p>
-                <h4>image : </h4><p class="name"><?php echo wp_get_attachment_url( get_post_thumbnail_id($result->ID), 'thumbnail' ); ?></p>
-                <h4>created : </h4><p class="name"><?php echo $result->post_date; ?></p>
-                <h4>modified : </h4><p class="name"><?php echo $result->post_modified; ?></p>
-                <h4>role :</h4><p class="name"> <?php echo get_post_meta($result->ID, 'role', true); ?></p>
-                <h4>Academic institution : </h4><p class="name"><?php echo get_post_meta($result->ID, 'academic_institution', true); ?></p>
-                <h4>Email : </h4><p class="name"><?php echo get_post_meta($result->ID, 'email', true); ?></p>
-    </div>
+    <div class="restaurant-card" style="border: 1px solid red; min-height: 150px; padding: 50px">
+        <h4>lecturer Number :</h4><p> <?php echo $i+1; ?></p>
+        <h4>Herbrew :</h4><p class="name"> <?php echo $resultHebrew? $resultHebrew->ID : null; ?></p>
+        <h4>English :</h4><p class="name"> <?php echo $resultEnglish? $resultEnglish->ID : null; ?></p>
+        <h4>Arabic :</h4><p class="name"> <?php echo $resultArabic? $resultArabic->ID : null; ?></p>
 
+        <h4>Herbrew Name:</h4><p class="name"> <?php echo $resultHebrew? $resultHebrew->post_title : null; ?></p>
+        <h4>English Name:</h4><p class="name"> <?php echo $resultEnglish? $resultEnglish->post_title : null; ?></p>
+        <h4>Arabic Name:</h4><p class="name"> <?php echo $resultArabic? $resultArabic->post_title : null; ?></p>
 
-        <?php
-}
-
-
-
-
-
-
-global $wpdb;
-$results = $wpdb->get_results( "SELECT * FROM $wpdb->posts WHERE `post_type`='lecturer'" ); // AND `post_title`='מוריה מרינה וקנין'
-
-$hebrew_ids = array();
-//foreach ( $results as $result )
-//{
-//    $translated = apply_filters( 'wpml_object_id', $result->ID, 'lecturer', FALSE, 'he' );
-//    if ($translated){
-//        array_push($hebrew_ids, $translated);
-//    }
-//}
-$unique_hebrew_ids = array_unique($hebrew_ids);
-
-$index=1;
-
-foreach ( $unique_hebrew_ids as $hebrew_id )
-{
-    $result = get_post($hebrew_id);
-    ?>
-
-    <div class="restaurant-card" style="border: 1px solid red; min-height: 150px; padding: 10px">
-        <h4>lecturer Number :</h4><p> <?php echo $index++; ?></p>
-        <!--        <h4>ID :</h4><p class="name"> --><?php //echo $result->ID; ?><!--</p>-->
-        <h4>Name : </h4><p class="name"><?php echo $result->post_title; ?></p>
-        <!--        <h4>Author ID :</h4><p class="name"> --><?php //echo $result->post_author; ?><!--</p>-->
-        <!--        <h4>Description : </h4><p class="name">--><?php //echo $result->post_content; ?><!--</p>-->
-        <!--        <h4>image : </h4><p class="name">--><?php //echo wp_get_attachment_url( get_post_thumbnail_id($result->ID), 'thumbnail' ); ?><!--</p>-->
-        <!--        <h4>created : </h4><p class="name">--><?php //echo $result->post_date; ?><!--</p>-->
-        <!--        <h4>modified : </h4><p class="name">--><?php //echo $result->post_modified; ?><!--</p>-->
-        <!--        <h4>role :</h4><p class="name"> --><?php //echo get_post_meta($result->ID, 'role', true); ?><!--</p>-->
-        <!--        <h4>Academic institution : </h4><p class="name">--><?php //echo get_post_meta($result->ID, 'academic_institution', true); ?><!--</p>-->
-        <!--        <h4>Email : </h4><p class="name">--><?php //echo get_post_meta($result->ID, 'email', true); ?><!--</p>-->
+        <!--                <h4>ID :</h4><p class="name"> --><?php //echo $result->ID; ?><!--</p>-->
+        <!--        <h4>Name : </h4><p class="name">--><?php //echo $result->post_title; ?><!--</p>-->
+        <!--                <h4>Author ID :</h4><p class="name"> --><?php //echo $result->post_author; ?><!--</p>-->
+        <!--                <h4>Description : </h4><p class="name">--><?php //echo $result->post_content; ?><!--</p>-->
+        <!--                <h4>image : </h4><p class="name">--><?php //echo wp_get_attachment_url( get_post_thumbnail_id($result->ID), 'thumbnail' ); ?><!--</p>-->
+        <!--                <h4>created : </h4><p class="name">--><?php //echo $result->post_date; ?><!--</p>-->
+        <!--                <h4>modified : </h4><p class="name">--><?php //echo $result->post_modified; ?><!--</p>-->
+        <!--                <h4>role :</h4><p class="name"> --><?php //echo get_post_meta($result->ID, 'role', true); ?><!--</p>-->
+        <!--                <h4>Academic institution : </h4><p class="name">--><?php //echo get_post_meta($result->ID, 'academic_institution', true); ?><!--</p>-->
+        <!--                <h4>Email : </h4><p class="name">--><?php //echo get_post_meta($result->ID, 'email', true); ?><!--</p>-->
     </div>
 
     <?php
 
 
-
-
-
-
-
     // get academic institution id
-    $academic_institution_id = get_post_meta($result->ID, 'academic_institution', true);
+    $academic_institution_id = get_post_meta($resultHebrew->ID, 'academic_institution', true);
     $academic_institution = get_post($academic_institution_id);
     $name = $academic_institution->post_title;
+    if(str_contains($name, '"'))
+        $name = str_replace('"','""', $name);
     $mypod = pods( 'academic_institution', array('where' => 't.name LIKE "%' . $name . '%"') );
     $PodAcademic_institution_id = $mypod->display( 'id' );
 
@@ -125,37 +79,58 @@ foreach ( $unique_hebrew_ids as $hebrew_id )
 
     if($PodAcademic_institution_id)
         $data = array(
-            'name' => $result->post_title,
-            'author' => $result->post_author,
-            'created' => $result->post_date,
-            'modified' => $result->post_modified,
-            'hebrew_description' => $result->post_content,
-            'hebrew_role' => get_post_meta($result->ID, 'role', true),
-            'email' => get_post_meta($result->ID, 'email', true),
-            'image' => get_post_thumbnail_id($result->ID),
+            'name' => $resultHebrew->post_title,
+            'english_name' => $resultEnglish ?  $resultEnglish->post_title : null,
+            'arabic_name' => $resultArabic ?  $resultArabic->post_title : null,
+
+            'author' => $resultHebrew->post_author,
+            'created' => $resultHebrew->post_date,
+            'modified' => $resultHebrew->post_modified,
+
+            'hebrew_description' => $resultHebrew->post_content,
+            'english_description' => $resultEnglish->post_content,
+            'arabic_description' => $resultArabic->post_content,
+
+            'hebrew_role' => get_post_meta($resultHebrew->ID, 'role', true),
+            'english_role' => $resultEnglish ? get_post_meta($resultEnglish->ID, 'role', true) : null,
+            'arabic_role' => $resultArabic ? get_post_meta($resultArabic->ID, 'role', true) : null,
+
+            'email' => get_post_meta($resultHebrew->ID, 'email', true),
+            'image' => get_post_thumbnail_id($resultHebrew->ID),
             'academic_institution' => $PodAcademic_institution_id,
         );
     else
         $data = array(
-            'name' => $result->post_title,
-            'author' => $result->post_author,
-            'created' => $result->post_date,
-            'modified' => $result->post_modified,
-            'hebrew_description' => $result->post_content,
-            'hebrew_role' => get_post_meta($result->ID, 'role', true),
-            'email' => get_post_meta($result->ID, 'email', true),
-            'image' => get_post_thumbnail_id($result->ID),
+            'name' => $resultHebrew->post_title,
+            'english_name' => $resultEnglish ?  $resultEnglish->post_title : null,
+            'arabic_name' => $resultArabic ?  $resultArabic->post_title : null,
+
+            'author' => $resultHebrew->post_author,
+            'created' => $resultHebrew->post_date,
+            'modified' => $resultHebrew->post_modified,
+
+            'hebrew_description' => $resultHebrew->post_content,
+            'english_description' => $resultEnglish->post_content,
+            'arabic_description' => $resultArabic->post_content,
+
+            'hebrew_role' => get_post_meta($resultHebrew->ID, 'role', true),
+            'english_role' => $resultEnglish ? get_post_meta($resultEnglish->ID, 'role', true) : null,
+            'arabic_role' => $resultArabic ? get_post_meta($resultArabic->ID, 'role', true) : null,
+
+            'email' => get_post_meta($resultHebrew->ID, 'email', true),
+            'image' => get_post_thumbnail_id($resultHebrew->ID),
         );
 
+    var_dump($data);
 
 // Add the new item now and get the new ID
 //    $pod = pods( 'lecturer' );
 //    $new_book_id = $pod->add( $data );
-
-
-
-
+//    }
 
 }
+
+
+
 
 wp_reset_postdata(); ?>
