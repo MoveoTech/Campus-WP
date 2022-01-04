5cc0c164f2a34572297354c1009c07715cb6ee8e
aliKhMoveo <alik@moveo.co.il>
2022-01-04 18:30:01 +0200

    add stripes to homepage

--- template-parts/Stripes/academic-institution-stripe.php
+++ template-parts/Stripes/academic-institution-stripe.php
@@ -6,54 +6,29 @@ $stripe = wp_parse_args(
 if(empty($stripe) || empty($stripe['id']) || empty($stripe['carousel']) || count($stripe['carousel']) < 1)
     return;
 
-$academic_institutions = pods( 'academic_institution');
-$academic_institutions->find(academicInstitutionsParams($stripe['carousel']));
+$academic_institutions = pods( 'academic_institution', podsParams($stripe['carousel']));
 
 ?>
 
-    <div class="home-page-institutions_stripe"  >
-        <div hidden id="<?php echo $stripe['id']; ?>" class="academic-institutions-ids" value="<?php  print_r(json_encode($stripe['carousel'])); ?>" ></div>
-        <?php
-        if(!empty($stripe['title']) && $stripe['title'] != ""){
-            ?>
-            <div class="institution-header">
-                <span></span>
-                <h1><?php echo $stripe['title'] ?></h1>
-            </div>
-        <?php } ?>
-        <div class="institutions-slider" >
-            <?php
-            while ($academic_institutions->fetch()) { // academic_institution Loop
-                $thumb = $academic_institutions->display('image');
-                if($thumb){
-                    ?>
-                    <div class="course-stripe-item" style="margin: auto">
-                        <div class="course-img" style="background-image: url(<?php echo $thumb ?>);  "></div>
-                    </div>
-                <?php } } ?>
+<div class="home-page-institutions_stripe"  >
+    <div hidden id="<?php echo $stripe['id']; ?>" class="academic-institutions-ids" value="<?php  print_r(json_encode($stripe['carousel'])); ?>" ></div>
+    <?php
+    if(!empty($stripe['title']) && $stripe['title'] != ""){
+        ?>
+        <div class="institution-header">
+            <span></span>
+            <h1><?php echo $stripe['title'] ?></h1>
         </div>
+    <?php } ?>
+    <div class="institutions-slider" >
+        <?php
+        while ($academic_institutions->fetch()) { // academic_institution Loop
+            $thumb = $academic_institutions->display('image');
+            if($thumb){
+                ?>
+                <div class="course-stripe-item" style="margin: auto">
+                    <div class="course-img" style="background-image: url(<?php echo $thumb ?>);  "></div>
+                </div>
+            <?php } } ?>
     </div>
-
-<?php
-function academicInstitutionsParams($academic_institutions_stripe){
-
-    $where = "t.id IN (";
-    $order = "FIELD(t.id,";
-
-    foreach($academic_institutions_stripe as $academic_institution ) {
-        $where = $where . $academic_institution . ",";
-        $order = $order . $academic_institution . ",";
-
-    }
-    $where = substr_replace($where, ")", -1);
-    $order = substr_replace($order, ")", -1);
-
-    $params = array(
-        'limit' => -1,
-        'where'=>$where,
-        'orderby'=> $order
-    );
-    return $params;
-
-}
-?>
+</div>
---
+++ template-parts/Stripes/stripeTypes.php
@@ -0,0 +1,153 @@
+<?php
+
+
+function getStripeType($stripeId){
+
+    $stripeType = get_field('stripe_type', $stripeId);
+
+    switch ($stripeType) {
+
+        case 'Academic Institution Stripe':{
+            AcademicInstitutionStripe($stripeId);
+            break;
+        }
+        case 'Narrative Stripe':{
+            nerativeCoursesStripe($stripeId);
+            break;
+        }
+
+        case 'Tags Stripe':
+            tagsStripe($stripeId);
+            break;
+
+        case 'Info Stripe':
+            infoStripe($stripeId);
+            break;
+
+        case 'Courses Stripe':
+            coursesStripe($stripeId);
+            break;
+
+    }
+}
+
+
+
+function academicInstitutionStripe($stripeId){
+
+    global $sitepress;
+
+    get_template_part('template', 'parts/Stripes/academic-institution-stripe',
+        array(
+            'args' => array(
+                'id'=>  $stripeId,
+                'title' => getFieldByLanguage(get_field('hebrew_title', $stripeId), get_field('english_title', $stripeId), get_field('arabic_title', $stripeId), $sitepress->get_current_language()),
+                'carousel' => get_field('academic_institution', $stripeId),
+            )
+        ));
+
+}
+
+
+function nerativeCoursesStripe($stripeId){
+
+    global $sitepress;
+
+    $title = getFieldByLanguage(get_field('hebrew_title', $stripeId), get_field('english_title', $stripeId), get_field('arabic_title', $stripeId), $sitepress->get_current_language());
+
+    $subTitle = getFieldByLanguage(get_field('hebrew_sub_title', $stripeId), get_field('english_sub_title', $stripeId), get_field('arabic_sub_title', $stripeId), $sitepress->get_current_language());
+
+
+    get_template_part('template', 'parts/Stripes/nerative-stripe',
+        array(
+            'args' => array(
+                'id' => $stripeId,
+                'image' => get_field('logo', $stripeId),
+                'title' => $title,
+                'subtitle' => $subTitle,
+                'carousel' => get_field('courses', $stripeId),
+            )
+        ));
+
+}
+
+
+
+function coursesStripe($stripeId){
+
+    global $sitepress;
+
+    $title = getFieldByLanguage(get_field('hebrew_title', $stripeId), get_field('english_title', $stripeId), get_field('arabic_title', $stripeId), $sitepress->get_current_language());
+
+    $subTitle = getFieldByLanguage(get_field('hebrew_sub_title', $stripeId), get_field('english_sub_title', $stripeId), get_field('arabic_sub_title', $stripeId), $sitepress->get_current_language());
+
+
+    get_template_part('template', 'parts/Stripes/courses-stripe',
+        array(
+            'args' => array(
+                'id' => $stripeId,
+                'title' => $title,
+                'subtitle' => $subTitle,
+                'carousel' => get_field('courses', $stripeId),
+            )
+        ));
+}
+
+
+function infoStripe($stripeId){
+
+    global $sitepress;
+
+    $title = getFieldByLanguage(get_field('hebrew_title', $stripeId), get_field('english_title', $stripeId), get_field('arabic_title', $stripeId), $sitepress->get_current_language());
+
+
+    get_template_part('template', 'parts/Stripes/info-stripe',
+        array(
+            'args' => array(
+                'id' => $stripeId,
+                'title' => $title,
+                'carousel' => get_field('info', $stripeId)["info_item"],
+            )
+        ));
+}
+
+
+
+function tagsStripe($stripeId){
+
+    global $sitepress;
+    $title = getFieldByLanguage(get_field('hebrew_title', $stripeId), get_field('english_title', $stripeId), get_field('arabic_title', $stripeId), $sitepress->get_current_language());
+
+    var_dump(get_field('tags', $stripeId));
+    get_template_part('template', 'parts/Stripes/tags-stripe',
+        array(
+            'args' => array(
+                'title' => $title,
+                'tags' => get_field('tags', $stripeId),
+            )
+        ));
+}
+
+function podsParams($tags_stripe)
+{
+
+    $where = "t.id IN (";
+    $order = "FIELD(t.id,";
+
+    foreach ($tags_stripe as $tag) {
+        $where = $where . $tag . ",";
+        $order = $order . $tag . ",";
+
+    }
+    $where = substr_replace($where, ")", -1);
+    $order = substr_replace($order, ")", -1);
+
+    $params = array(
+        'limit' => -1,
+        'where' => $where,
+        'orderby' => $order
+    );
+    return $params;
+
+}
+
