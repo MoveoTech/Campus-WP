88b2ba07625fcc0592852e69127b4543c8ec7392
IdoLeybo <Idol@moveo.co.il>
2022-01-10 11:35:26 +0200

    Finished info stripe (Added url link)

--- template-parts/Stripes/info-stripe.php
+++ template-parts/Stripes/info-stripe.php
@@ -26,13 +26,17 @@ if(empty($stripe) || empty($stripe['carousel']) || count($stripe['carousel']) < 
                 $title = getFieldByLanguage($info_item['title']['hebrew_title'], $info_item['title']['english_title'], $info_item['title']['arabic_title'], $sitepress->get_current_language());
 
                 $subTitle = getFieldByLanguage($info_item['sub_title']['hebrew_sub_title'], $info_item['sub_title']['english_sub_title'], $info_item['sub_title']['arabic_sub_title'], $sitepress->get_current_language());
+
+                $url = $info_item['link'];
                 ?>
                 <div class="info-item" >
-                    <img src="<?php echo $info_item['info_media']['url'] ?>">
-                    <h1><?php echo $title ?></h1>
-                    <?php if($subTitle && $subTitle !== '') :?>
-                        <p ><?php echo $subTitle ?></p>
-                    <?php endif; ?>
+                    <a href="<?php echo $url ?>">
+                        <img src="<?php echo $info_item['info_media']['url'] ?>">
+                        <h1><?php echo $title ?></h1>
+                        <?php if($subTitle && $subTitle !== '') :?>
+                            <p ><?php echo $subTitle ?></p>
+                        <?php endif; ?>
+                    </a>
                 </div>
             <?php endforeach;
         endif;
