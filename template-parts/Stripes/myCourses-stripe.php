83a6899f41f598db625765d2dde07f86c9a98866
IdoLeybo <Idol@moveo.co.il>
2022-01-25 18:08:32 +0200

    Added My Courses Stripe structure & CSS

--- template-parts/Stripes/myCourses-stripe.php
+++ template-parts/Stripes/myCourses-stripe.php
@@ -14,13 +14,28 @@ if(empty($stripe) || empty($stripe['id']) || empty($stripe['courses']) || count(
             <h1><?php echo $stripe['title'] ?></h1>
         <?php endif; ?>
     </div>
-    <div class="myCourses-slider">
+    <div id="<?php echo $stripe['id'] ?>" class="myCourses-slider">
         <?php
 
         if($stripe['courses'] && count($stripe['courses']) > 0):
             foreach ($stripe['courses'] as $my_item) :
+                $academic_institution = pods( 'academic_institution', podsParams($my_item['academic_institution']));
+                $institution_name = '';
+                while ($academic_institution->fetch()):
+                    $institution_name = $academic_institution->display('name');
+                endwhile;
+                $title = $my_item['name'];
+                $thumb = $my_item['image']['url'];
         ?>
-                <img src="<?php echo $my_item['course_image']['url'] ?>" >
+                <div class="course-stripe-item" style="display: flex;flex-direction: column">
+                    <div class="course-img" style="background-image: url(<?= $thumb ?>);">
+                    </div>
+                    <div class="item-content">
+                        <p class="course-progress"><a href="<?= $url ?>">בואו נתחיל את הקורס >></a></p>
+                        <h3 ><a href="<?= $url ?>"><?= $title ?></a></h3>
+                        <p class="institution-name"><?= $institution_name?></p>
+                    </div>
+                </div>
             <?php endforeach;
         endif;
         ?>
