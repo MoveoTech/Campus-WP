61af4d481b3d47a78032482992a7f280d585fbdf
SapirAbutbulMoveo <sapira@moveo.co.il>
2022-03-09 18:22:05 +0200

    no message

--- functions.php
+++ functions.php
@@ -1283,3 +1283,12 @@ function CompareTagsByOrder($tag1, $tag2) {
     return $tag1['order'] > $tag2['order'];
 }
 
+// Daniel Kisos's function - console log in PHP
+function console_log($output, $with_script_tags = true) {
+    $js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) .
+        ');';
+    if ($with_script_tags) {
+        $js_code = '<script>' . $js_code . '</script>';
+    }
+    echo $js_code;
+}
--- template-parts/Filters/certificate-filter.php
+++ template-parts/Filters/certificate-filter.php
@@ -1,4 +1,5 @@
 <?php
+
 $filter = wp_parse_args(
     $args["args"]
 );
@@ -13,6 +14,41 @@ $title = $filter['title'];
 $certificates = [];
 foreach ($certificatesArray as $certificate) {
     $certificate_name = getFieldByLanguageFromString($certificate, $sitepress->get_current_language());
+
     array_push($certificates, $certificate_name);
 }
+//var_dump($certificates);
+
+$title = $filter['title'];
+$count = count($certificates);
 ?>
+
+<div class="wrap-terms-group">
+    <h2 class="search-page-tax-name"><?= $title ?></h2>
+    <div class="more-tags">
+
+        <?php foreach ($certificates as $certificate) {
+            $checked = '';
+            //TODO get all courses that have this tag -> $count = result.
+            ?>
+
+            <div class="wrap-filter-search">
+                <label class="term-filter-search" for="<?= $certificate ?>">
+                    <input <?= $checked ?> class="checkbox-filter-search" type="checkbox" data-name=' $language ' data-value="<?= $certificate ?>" name=" $language '[]'" value="<?= $certificate ?>" id="<?= $certificate ?>"/>
+                    <div class="wrap-term-and-sum" >
+                        <span class="term-name"><?= $certificate ?></span>
+                        <span class="sum">(<?= $count ?>)</span>
+                    </div>
+                </label>
+            </div>
+
+            <?php if (count($certificates) > 7) {?>
+                <button class="show-more-tags collapsed" type="button" aria-hidden="true">
+                    <span><?= __('Show More Tags', 'single_corse') ?></span>
+                    <span><?= __('Show Less Tags', 'single_corse') ?></span>
+                </button>
+
+
+            <?php } } ?>
+    </div>
+</div>
