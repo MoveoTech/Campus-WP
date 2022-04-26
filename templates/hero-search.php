0a180eb3724353f65ce8500d77ea6fbffa35e1f0
IdoLeybo <Idol@moveo.co.il>
2022-04-26 13:28:15 +0300

    Changed slug of catalog page

--- templates/hero-search.php
+++ templates/hero-search.php
@@ -1,4 +1,4 @@
-<form role="search" class="hero-search-form" action="<?= esc_url(home_url('/new-courses')); ?>" novalidate>
+<form role="search" class="hero-search-form" action="<?= esc_url(home_url('/catalog')); ?>" novalidate>
     <label class="sr-only"><?php _e('Search for:', 'single_corse'); ?></label>
     <div class="input-group group-search-form">
         <input type="search" value="<?= get_search_query(); ?>" name="text_s" class="search-field form-control" placeholder="<?php echo hero_search_placeholder(); ?>" aria-required="true">
