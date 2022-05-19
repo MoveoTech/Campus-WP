d125d872540ea9b046a07a8fd288c924cead1226
IdoLeybo <Idol@moveo.co.il>
2022-05-19 15:50:36 +0300

    Added Class to body element in single course page

--- course-single-page.php
+++ course-single-page.php
@@ -11,7 +11,7 @@
 <html class="no-js" <?php language_attributes(); ?>>
 <?php get_template_part('templates/head'); ?>
 
-<body <?php body_class(); ?>>
+<body class="single-course" <?php body_class(); ?>>
 <!--[if lt IE 9]>
 <div class="alert alert-warning">
     <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'sage'); ?>
